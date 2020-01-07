138f2d74af47f246271094f6313fed0b7f0b11d0
Isaac Vorköper <isaaclennart@gmail.com>
2020-01-07 08:52:49 +0100

    test

---
+++ Terningkast/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terningkast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int res = dice.Next(1, 7);
+            int i = 1;
+
+            while(res != 6){
+                res = dice.Next(1, 7);
+                i++;
+            }
+            Console.WriteLine("Det tog #{0:D} gange at slå en sekser", i);
+            }
+        }
+    }
